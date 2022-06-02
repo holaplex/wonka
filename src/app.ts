81ac2e834bf29d356de26b47fc0d6e62fe589f4b
Kevin Rodriguez <_@kevinrodriguez.io>
2022-06-02 12:34:56 -0600

    feat: cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import { makeSchema } from 'nexus';
 import { createServer } from 'graphql-yoga';
-import { CreateUserMutation, CreateUserMutationResult } from './graphql/user.js';
+import * as user from './graphql/user.js';
 import path from 'path';
 const dirname = path.resolve();
 
 const PORT = parseInt(process.env!.PORT, 10);
 
 const schema = makeSchema({
-  types: [CreateUserMutationResult, CreateUserMutation],
+  types: [user],
   outputs: {
     schema: dirname + '/generated/schema.graphql',
     typegen: dirname + '/generated/typings.ts',
@@ -17,6 +17,6 @@ const schema = makeSchema({
 const server = createServer({ schema, port: PORT });
 
 server.start().then(() => {
-  console.log('dirname: ', dirname);
+  console.log('ENV: X ', process.env.FIREBASE_CONFIG!);
   console.log(`🎸 Ready to rock on: ${PORT}`);
 });
