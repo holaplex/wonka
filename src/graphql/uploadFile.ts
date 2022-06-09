af9f7934f740bd4b42620cbb5a9772a7d51175f8
Jason Schorr <echohtp@gmail.com>
2022-06-09 17:58:33 -0400

    finish stream stuff

--- src/graphql/uploadFile.ts
+++ src/graphql/uploadFile.ts
@@ -2,14 +2,15 @@ import { nonNull, mutationField, arg, objectType, scalarType } from 'nexus';
 import { YogaInitialContext } from 'graphql-yoga';
 import { NFTStorage } from 'nft.storage';
 import fs from 'fs';
-export const fromDwebLink = (cid: string): string => `https://${cid}.ipfs.dweb.link`;
+export const fromDwebLink = (cid: string): string =>
+  `https://${cid}.ipfs.dweb.link`;
 
 export const UploadFileResult = objectType({
   name: 'UploadFileResult',
   description: 'The result for uploading a file',
   definition (t) {
     t.nonNull.string('message', {
-      description: 'Upload URL of the file',
+      description: 'Upload url of the file',
     });
   },
 });
@@ -25,14 +26,24 @@ export const UploadFile = mutationField('uploadFile', {
   type: 'UploadFileResult',
   args: { file: nonNull(arg({ type: 'Upload' })) },
   async resolve (_, args, ctx: YogaInitialContext) {
-    const nftstorage = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY });
-    const { file } = await args.file;
-    const data = await fs.promises.readFile(file.path);
-    const cid = await nftstorage.storeBlob(new Blob([data]));
-    const status = await nftstorage.status(cid)
-    console.log(status)
-    return {
-      message: fromDwebLink(cid)
-    };
+    const nftstorage = new NFTStorage({
+      token: process.env.NFT_STORAGE_API_KEY,
+    });
+    let data = '';
+    const stream = await args.file.stream();
+    stream.on('data', (chunk) => {
+      data += chunk;
+    });
+    stream.on('end', async () => {
+      const cid = await nftstorage.storeBlob(new Blob([data]));
+      return {
+        message: fromDwebLink(cid),
+      };
+    });
+    stream.on('error', (e) => {
+      return {
+        message: `Error uploading file: ${e.message}`,
+      };
+    });
   },
 });
