9349a1356c256f0cc7f41f62f7a54a31a9baeb49
William Barksdale <wfbarksdale@gmail.com>
2022-09-09 16:40:47 -0500

    dedupe download logic

--- src/lib/helpers/downloadFile.ts
+++ src/lib/helpers/downloadFile.ts
@@ -8,49 +8,35 @@ export const download = (url: string, dest: string) =>
     fs.access(dest, fs.constants.F_OK, (err) => {
       if (err === null) reject('File already exists');
 
+      const responseHandler = (response: http.IncomingMessage): void => {
+        if (response.statusCode === 200) {
+          const file = fs.createWriteStream(dest, { flags: 'wx' });
+          file.on('finish', () => resolve());
+          file.on('error', (err: any) => {
+            file.close();
+            if (err.code === 'EEXIST') reject('File already exists');
+            else fs.unlink(dest, () => reject(err.message)); // Delete temp file
+          });
+          response.pipe(file);
+        } else if (response.statusCode === 302 || response.statusCode === 301) {
+          //Recursively follow redirects, only a 200 will resolve.
+          download(response.headers.location, dest).then(() => resolve());
+        } else {
+          reject(
+            `Server responded with ${response.statusCode}: ${response.statusMessage}`,
+          );
+        }
+      };
+
+      let request: http.ClientRequest;
       if (url.startsWith('https')) {
-        const request = https.get(url, (response) => {
-          if (response.statusCode === 200) {
-            const file = fs.createWriteStream(dest, { flags: 'wx' });
-            file.on('finish', () => resolve());
-            file.on('error', (err: any) => {
-              file.close();
-              if (err.code === 'EEXIST') reject('File already exists');
-              else fs.unlink(dest, () => reject(err.message)); // Delete temp file
-            });
-            response.pipe(file);
-          } else if (
-            response.statusCode === 302 ||
-            response.statusCode === 301
-          ) {
-            //Recursively follow redirects, only a 200 will resolve.
-            download(response.headers.location, dest).then(() => resolve());
-          } else {
-            reject(
-              `Server responded with ${response.statusCode}: ${response.statusMessage}`,
-            );
-          }
-        });
-        request.on('error', (err) => {
-          reject(err.message);
-        });
+        request = https.get(url, responseHandler);
       } else {
-        const request = http.get(url, (response) => {
-          console.log('Download Response: ', response.statusCode);
-          if (response.statusCode == 200) {
-            const file = fs.createWriteStream(dest, { flags: 'wx' });
-            file.on('finish', () => resolve());
-            file.on('error', (err: any) => {
-              file.close();
-              if (err.code === 'EEXIST') reject('File already exists');
-              else fs.unlink(dest, () => reject(err.message)); // Delete temp file
-            });
-            response.pipe(file);
-          }
-        });
-        request.on('error', (err) => {
-          reject(err.message);
-        });
+        request = http.get(url, responseHandler);
       }
+
+      request.on('error', (err) => {
+        reject(err.message);
+      });
     });
   });
